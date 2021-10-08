abebbd2f7f5bf460bacad9e037ffe7606017c025
doctorzorka <52214587+DoctorZorka@users.noreply.github.com>
2021-10-07 18:33:32 -0700

    Create xss-fuzz.py
    simple script to fuzz for XSS

---
+++ xss-fuzz.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import requests
+
+
+fd = open("xss-payloads.txt", "r")
+url = "http://3.challenge.xss.site/search.php"
+
+
+
+while True:
+    #read each line from xss-payloads.txt
+    payload = fd.readline()
+    #check EOF
+    if payload == "":
+        break
+    
+    response = requests.get(url,params={'term': payload})
+    #check if XSS is found on webpage
+    if resp.text.find(payload) != -1:
+        print("XSS Found:! " + payload)
